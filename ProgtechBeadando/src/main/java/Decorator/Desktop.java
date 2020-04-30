40f108f5bef4e88d737969afdc101b1bdecf09e9
Wh0Am1 <balintb99@gmail.com>
2020-04-30 14:58:12 +0200

    Minor changes in Desktop.java
    Added extra condition handling by using ifs

--- ProgtechBeadando/src/main/java/Decorator/Desktop.java
+++ ProgtechBeadando/src/main/java/Decorator/Desktop.java
@@ -1,10 +1,12 @@
 package Decorator;
 
 import ComputerParts.CPU.CPU;
-import ComputerParts.Motherboard.Motherboard;
+import ComputerParts.Motherboard.DesktopMotherboard;
 import ComputerParts.RAM.RAM;
 import ComputerParts.Screen.Screen;
 import ComputerParts.PSU.PSU;
+import Exceptions.InvalidMotherboardException;
+import Exceptions.InvalidPSUException;
 
 /**
  *
@@ -12,21 +14,35 @@ import ComputerParts.PSU.PSU;
  */
 public class Desktop extends Computer{
 
-    private PSU psu;
-    private Screen screen;
-    private DesktopMotherboard motherboard;
-    private RAM ram;
     private CPU cpu;
+    private DesktopMotherboard motherboard;
+    private PSU psu;
+    private RAM ram;
+    private Screen screen;  
+    private ComputerCase pcCase;
     
+    public Computer desktop = new Computer();
     
-    
-    public Desktop(ComputerCase pcCase) {
+    public Desktop(CPU cpu, DesktopMotherboard motherboard, PSU psu, RAM ram, Screen screen, ComputerCase pcCase) throws InvalidPSUException, InvalidMotherboardException {
         super(pcCase);
+        this.cpu = cpu;
+        this.motherboard = motherboard;
+        this.psu = psu;
+        this.ram = ram;
+        this.screen = screen;
+        
+        if(this.psu.getPsuType().Desktop_PSU != psu.getPsuType()){
+            throw new InvalidPSUException("This psu is not compatible with a desktop! This is a laptop PSU!");
+        }
+        if (this.motherboard.getMotherBoardType().Desktop != motherboard.getMotherBoardType()) {
+            throw new InvalidMotherboardException("This motherboard is not compatible with a desktop! This is a laptop Motherboard!");
+        }
     }
+    
     
     @Override
     public String Build() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return "PC parts: CPU - " + this.cpu.getCores() + " Motherboard - " + this.motherboard.getMotherBoardSize() + " PSU - " + this.psu.getPower() + " RAM - " + this.ram.getSize() + " GB " + " Screen - " + this.screen.getResolution() + " Computer Case - " + this.pcCase;
     }
     
     
