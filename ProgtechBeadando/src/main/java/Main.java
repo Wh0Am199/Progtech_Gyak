1be95d3e3f042be00b6746e6d94ca94b21d6b43c
Wh0Am1 <balintb99@gmail.com>
2020-04-30 10:50:48 +0200

    Deleted replaced files
    Deleted form, added main.java

---
+++ ProgtechBeadando/src/main/java/Main.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author Bárdos Bálint
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        ShoppingBasket basket = ShoppingBasket.getInstance();
+    }
+    
+}
