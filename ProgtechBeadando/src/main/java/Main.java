18963158fa7495c49adda2078c20f125cddcd87f
Wh0Am1 <balintb99@gmail.com>
2020-05-12 12:47:08 +0200

    Update in Main.java
    Put the whole code in a try catch

--- ProgtechBeadando/src/main/java/Main.java
+++ ProgtechBeadando/src/main/java/Main.java
@@ -15,6 +15,7 @@ import ComputerParts.Screen.Screen;
 import Decorator.ComputerCase;
 import Decorator.Desktop;
 import Decorator.Laptop;
+import Exceptions.InvalidCaseException;
 import Exceptions.InvalidMotherboardException;
 import Exceptions.InvalidPSUException;
 import PSU.PsuType;
@@ -25,32 +26,44 @@ import PSU.PsuType;
  */
 public class Main {
 
-    public static void main(String[] args) throws InvalidPSUException, InvalidMotherboardException {
+    public static void main(String[] args) throws InvalidPSUException, InvalidMotherboardException, InvalidCaseException {
         
-        ShoppingBasket basket = ShoppingBasket.getInstance();
+        try {
+            ShoppingBasket basket = ShoppingBasket.getInstance();
         
-        CPU cpu = new CPU(8,CpuSocket.Socket_1150);
-        GPU gpu = new GPU(GpuType.NVIDIA,6,500);
-        LaptopMotherboard mb = new LaptopMotherboard();
-        PSU psu = new PSU(PsuType.Laptop_Battery, 600);
-        RAM ram = new RAM(RamType.DDR3,8);
-        Screen screen = new Screen(AspectRatio._16_9, Resolution.FULL_HD);
+            CPU cpu = new CPU(8,CpuSocket.Socket_1150);
+            GPU gpu = new GPU(GpuType.NVIDIA,6,500);
+            LaptopMotherboard mb = new LaptopMotherboard();
+            PSU psu = new PSU(PsuType.Laptop_Battery, 600);
+            RAM ram = new RAM(RamType.DDR3,8);
+            Screen screen = new Screen(AspectRatio._16_9, Resolution.FULL_HD);
+
+            Laptop laptop = new Laptop(cpu,gpu,mb,psu,ram,screen,ComputerCase.Laptop);
+            laptop.Build();
+            basket.Add(laptop);
+
+
+            CPU cpu2 = cpu.DeepCopy();
+            GPU gpu2 = new GPU(GpuType.AMD,4,650);
+            DesktopMotherboard mb2 = new DesktopMotherboard(MotherboardSize.ATX);
+            PSU psu2 = new PSU(PsuType.Desktop_PSU, 750);
+            RAM ram2 = new RAM(RamType.DDR4,16);
+            Screen screen2 = new Screen(AspectRatio._21_9, Resolution.QHD);
+
+            Desktop desktop = new Desktop(cpu2,gpu2,mb2,psu2,ram2,screen2,ComputerCase.ATX);
+            desktop.Build();
+            basket.Add(desktop);
+        } catch (InvalidPSUException e){
+            e.printStackTrace();
+        } catch (InvalidMotherboardException emb){
+            emb.printStackTrace();
+        } catch (InvalidCaseException ic){
+            ic.printStackTrace();
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
         
-        Laptop laptop = new Laptop(cpu,gpu,mb,psu,ram,screen,ComputerCase.ATX);
-        laptop.Build();
-        basket.Add(laptop);
         
-        
-        CPU cpu2 = cpu.DeepCopy();
-        GPU gpu2 = new GPU(GpuType.AMD,4,650);
-        DesktopMotherboard mb2 = new DesktopMotherboard(MotherboardSize.ATX);
-        PSU psu2 = new PSU(PsuType.Desktop_PSU, 750);
-        RAM ram2 = new RAM(RamType.DDR4,16);
-        Screen screen2 = new Screen(AspectRatio._21_9, Resolution.QHD);
-        
-        Desktop desktop = new Desktop(cpu2,gpu2,mb2,psu2,ram2,screen2,ComputerCase.ATX);
-        desktop.Build();
-        basket.Add(desktop);
     }
     
 }
